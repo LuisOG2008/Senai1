1d9f596f535f65465577977d1c9ef49926e6527f
LuisOG2008 <luis_gubert@estudante.sesisenai.org.br>
2026-02-18 20:05:45 +0000

    update

---
+++ atividadepython/atividade1.py
@@ -0,0 +1,7 @@
+a = input( "Insira um numero:")
+b = input (" Insira o segundo numero:")
+print (a+b)
+
+a = float (input( "Insira um numero:"))
+b = float (input (" Insira o segundo numero:"))
+print (a+b)
